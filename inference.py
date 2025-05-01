7cc83b5cfb222e15c00d4f492aa3fa65dc73dbed
vdmello17 <159818669+vdmello17@users.noreply.github.com>
2025-04-30 23:53:56 -0400

    Update inference.py

--- inference.py
+++ inference.py
@@ -78,7 +78,7 @@ model = LSTMWithMetadataAttention(
 
 
 MODEL_PATH = "fake_news_model.pth"
-MODEL_URL = "https://YOUR_FILE_LINK_HERE"  # Replace with a direct download link
+MODEL_URL = "https://drive.google.com/file/d/1OqcWVx2BOnBIixfiE4PxUwbP2Dp4Xr_6/view?usp=sharing"  # Replace with a direct download link
 
 if not os.path.exists(MODEL_PATH):
     print("Downloading model...")
